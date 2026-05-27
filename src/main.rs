8c9afce7786d27e423fc974bf4069a3e6f634643
kabwoyke <edwardkabwoy@gmail.com>
2026-05-27 10:07:40 +0300

    complete minigrep impl

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,19 @@
+use minigrep::Config;
 use std::env;
-use std::fs;
 use std::process;
 
 fn main() {
-    let args:Vec<String> = env::args().collect();
-    
+    let args: Vec<String> = env::args().collect();
+
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing argument:{}" , err);
+        eprintln!("Problem parsing argument:{}", err);
         process::exit(1);
     });
-    
-    println!("{}" , config.query); 
-    println!("{}" , config.file_path);
-    let contents = fs::read_to_string(config.file_path).expect("Failed to read file contents");
-    println!("{}" , contents);
-}
 
-
-struct Config {
-    query:String,
-    file_path:String,
-}
-
-impl Config {
-    fn build (args:&[String]) -> Result<Config , &'static str> {
-    if args.len() < 3 {
-        return Err("Not Enough arguments")
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Appplication Error: {}", e);
+        process::exit(1)
     }
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Ok(Config {query , file_path})
-
 }
-}
+
+
